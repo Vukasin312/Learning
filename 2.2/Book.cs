a1632fe97c98a4110d612980fcbc4a34c73cc312
Vukasin312 <rexxar.simic66@gmail.com>
2023-03-06 16:13:35 +0100

    Conflict

--- 2.2/Book.cs
+++ 2.2/Book.cs
@@ -39,6 +39,7 @@
 		public string Render()
 		{
 			// dodaj da radi sa svi autori
+			// bilo sta
 			return "Book[name = " + _name + _authors[0].Render() + _authors[1].Render() + "price = " + _price + "qty = " + _qty + "]";
 		}
 	}
