5e8cb48e1a78ae202afda52d6aa607f90b3b0bd3
Vukasin312 <rexxar.simic66@gmail.com>
2023-03-06 16:26:20 +0100

    asd

--- 2.2/Program.cs
+++ 2.2/Program.cs
@@ -9,8 +9,7 @@
 			Console.WriteLine(test);
 			Author[] authors = new Author[2];
 			authors[0] = new Author("Vukasin", "rexxar.simic66@gmail.com", 'm');
-			authors[1] = new Author("Tijana", "tijana@gmail.com", 'f');		
-			// komentar
+			authors[1] = new Author("Tijana", "tijana@gmail.com", 'f');					
 			Book book = new Book("Knjiga", authors, 19.99, 99);
 
 			book.GetAuthorName();
