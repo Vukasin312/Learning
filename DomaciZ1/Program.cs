904daaf1f619a2ed81f2e3a81efb9d7c4ed610de
Petar <Petar@DESKTOP-E2ENA3G>
2023-03-11 03:16:00 +0100

    more UTs

--- DomaciZ1/Program.cs
+++ DomaciZ1/Program.cs
@@ -6,6 +6,7 @@
 		{
 			Interval interval1 = new Interval(6, 12, 25, true);
 			Interval interval2 = new Interval(6, 12, 55, true);
+
 			// nije dobro ne treba ti ovaj
 			// kad napravis da ima 1 argument ce ti radi bez ovaj
 			Interval newInterval = new Interval(0, 0, 0, true);
--- DomaciZ1Tests/Test.cs
+++ DomaciZ1Tests/Test.cs
@@ -16,6 +16,17 @@ namespace DomaciZ1Tests
             Assert.AreEqual(4, i1.GetHours(0));
             Assert.AreEqual(25, i1.GetMinutes(0));
         }
+
+        [TestMethod]
+        public void AddInterval_GoesOver60Mins_ReturnsOk()
+        {
+            Interval i1 = new(5, 3, 10, true);
+            Interval i2 = new(2, 1, 55, true);
+            i1.Add(i2);
+            Assert.AreEqual(7, i1.GetDays(0));
+            Assert.AreEqual(5, i1.GetHours(0));
+            Assert.AreEqual(25, i1.GetMinutes(0));
+        }
         [TestMethod]
         public void SubstractInterval_ReturnsOk()
         {
