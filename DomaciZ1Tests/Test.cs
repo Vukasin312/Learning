df169346dc3ccd9130548e2b9900d93e5c575cce
Petar <Petar@DESKTOP-E2ENA3G>
2023-03-14 20:56:15 +0100

    UT fix

--- DomaciZ1Tests/Test.cs
+++ DomaciZ1Tests/Test.cs
@@ -11,10 +11,10 @@ namespace DomaciZ1Tests
         {
             Interval i1 = new(5, 3, 10, true);
             Interval i2 = new(2, 1, 15, true);
-            i1.Add(i2);
-            Assert.AreEqual(7, i1.GetDays());
-            Assert.AreEqual(4, i1.GetHours());
-            Assert.AreEqual(25, i1.GetMinutes());
+            var result = i1.Add(i2);
+            Assert.AreEqual(7, result.GetDays());
+            Assert.AreEqual(4, result.GetHours());
+            Assert.AreEqual(25, result.GetMinutes());
         }
 
         [TestMethod]
@@ -22,39 +22,41 @@ namespace DomaciZ1Tests
         {
             Interval i1 = new(5, 3, 10, true);
             Interval i2 = new(2, 1, 55, true);
-            i1.Add(i2);
-            Assert.AreEqual(7, i1.GetDays());
-            Assert.AreEqual(5, i1.GetHours());
-            Assert.AreEqual(5, i1.GetMinutes());
+            var result = i1.Add(i2);
+            Assert.AreEqual(7, result.GetDays());
+            Assert.AreEqual(5, result.GetHours());
+            Assert.AreEqual(5, result.GetMinutes());
         }
         [TestMethod]
         public void SubstractInterval_ReturnsOk()
         {
             Interval i1 = new(5, 3, 10, true);
             Interval i2 = new(2, 1, 15, true);
-            i1.Subtract(i2);
-            Assert.AreEqual(3, i1.GetDays());
-            Assert.AreEqual(1, i1.GetHours());
-            Assert.AreEqual(55, i1.GetMinutes());
+            var result = i1.Subtract(i2);
+            Assert.AreEqual(3, result.GetDays());
+            Assert.AreEqual(1, result.GetHours());
+            Assert.AreEqual(55, result.GetMinutes());
         }
         [TestMethod]
         public void AddInterval_GoesOver60MinsAnd24Hours_ReturnsOk()
         {
             Interval i1 = new(5, 23, 10, true);
             Interval i2 = new(2, 1, 55, true);
-            i1.Add(i2);
-            Assert.AreEqual(8, i1.GetDays());
-            Assert.AreEqual(1, i1.GetHours());
-            Assert.AreEqual(5, i1.GetMinutes());
+            var result = i1.Add(i2);
+            Assert.AreEqual(8, result.GetDays());
+            Assert.AreEqual(1, result.GetHours());
+            Assert.AreEqual(5, result.GetMinutes());
         }
 
         [TestMethod]
-        public void AddInterval_1s_ReturnsOk()
+        public void AddInterval_Exaclty60MinsAnd24Hours_ReturnsOk()
         {
-            var test = new Interval(28, 20, 33, true).Add(new Interval(28, 3, 32, true));
-            Assert.AreEqual(57, test.GetDays());
-            Assert.AreEqual(0, test.GetHours());
-            Assert.AreEqual(5, test.GetMinutes());
+            Interval i1 = new Interval(28, 20, 30, true);
+            Interval i2 = new Interval(28, 3, 30, true);
+            var result = i1.Add(i2);
+            Assert.AreEqual(57, result.GetDays());
+            Assert.AreEqual(0, result.GetHours());
+            Assert.AreEqual(5, result.GetMinutes());
         }
     }
 }
